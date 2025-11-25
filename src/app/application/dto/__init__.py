2c15c7da75b627ee2022c598538346e6e041170b
medniy2000 <Medniy2000@users.noreply.github.com>
2025-11-25 19:12:41 +0200

    optimize imports

--- src/app/application/dto/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.app.application.dto.auth import DecodedTokenDTO, TokenPairDTO
-from src.app.application.dto.user import CreateUserByEmailDTO, CreateUserByPhoneDTO, UserShortDTO
-
-__all__ = ["UserShortDTO", "TokenPairDTO", "DecodedTokenDTO", "CreateUserByEmailDTO", "CreateUserByPhoneDTO"]
